c424a0a98d4a880663bf53b086554b6665de956e
uwx <cadrekucra@gmx.com>
2023-04-09 03:34:59 -0300

    fix robots.txt for real this time

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,5 +1,8 @@
 <svelte:head>
     <title>Search</title>
     <meta name="description" content="Search" />
+    <script async src="https://cse.google.com/cse.js?cx=a69bfdd3beb524c56">
+    </script>
 </svelte:head>
 
+<div class="gcse-search"></div>
