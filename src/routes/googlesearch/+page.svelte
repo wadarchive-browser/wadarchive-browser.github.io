1451ad0c5908e2cf34abb76aecab1ac5c2ca85a4
uwx <cadrekucra@gmx.com>
2023-04-10 02:27:17 -0300

    google search endpoint

---
+++ src/routes/googlesearch/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+    <title>Search</title>
+    <meta name="description" content="Search" />
+    <!--<script async src="https://cse.google.com/cse.js?cx=a69bfdd3beb524c56"></script>-->
+</svelte:head>
+
+<div class="gcse-search" />
